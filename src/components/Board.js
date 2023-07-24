1a9b873b52c6577ee3facc02e1c4a55714c841fd
ddengmin <ammin1127@naver.com>
2023-07-24 17:33:32 +0900

    ✅  "O,X 빙고를 통한 승자, 패자 정하기 기능 추가"

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,11 +5,54 @@ import "./Board.css";
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   //getter, setter
+  const [xIsNext, setxIsNext] = useState(true);
+  //getter, setter
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+      //승자 경우의 수
+    ];
+    for (let index = 0; index < lines.length; index++) {
+      const [a, b, c] = lines[index];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner : " + winner;
+  } else {
+    status = "Next Player : " + (xIsNext ? "X" : "O");
+  }
 
   const handleClick = (i) => {
     const newSquares = squares.slice();
-    newSquares[i] = "X";
+    console.log("newSquares", newSquares);
+    console.log("newSquares[i]", newSquares[i]);
+    if (calculateWinner(newSquares) || squares[i]) {
+      return;
+    }
+
+    newSquares[i] = xIsNext ? "X" : "O";
     setSquares(newSquares);
+    setxIsNext((prev) => !prev);
+    //삼항연산자를 사용 그 이후, 전에 떴던 모양을 안뜨게 해줌
   };
   //함수형 컴포넌트로 바꿔줌 #1
 
@@ -20,7 +63,7 @@ const Board = () => {
 
   return (
     <div>
-      <div className="status">Next Player : X,O</div>
+      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
