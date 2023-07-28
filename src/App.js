542ec27fe48e4a0fa36006f256fd51e52eb55b60
ddengmin <ammin1127@naver.com>
2023-07-28 17:17:58 +0900

    ✅ "add : JumpTo 함수를 추가하여 과거로 돌아가기

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
   const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
   const [xIsNext, setXIsNext] = useState(true);
   //board.js에 있는 아이들을 app.js로 옮겨줌(위에 두개)
+  const [stepNumber, setStepNumber] = useState(0);
+  //현재 몇번째 스탭인지 알 수 있는 state 생성하기.
   const calculateWinner = (squares) => {
     const lines = [
       [0, 1, 2],
@@ -31,7 +33,8 @@ function App() {
     return null;
   };
 
-  const current = history[history.length - 1];
+  const current = history[stepNumber];
+  //stepnumber로 바꾸기.
   const winner = calculateWinner(current.squares);
 
   let status;
@@ -42,17 +45,37 @@ function App() {
   }
 
   const handleClick = (i) => {
-    const newSquares = current.squares.slice();
+    const newHistory = history.slice(0, stepNumber + 1);
+    //slice를 이용해 배열 복사
+    const newCurrent = newHistory[newHistory.length - 1];
+    //history의 배열중 현재 아이템만
+    const newSquares = newCurrent.squares.slice();
     if (calculateWinner(newSquares) || newSquares[i]) {
       return;
     }
 
     newSquares[i] = xIsNext ? "X" : "O";
-    setHistory([...history, { squares: newSquares }]);
+    setHistory([...newHistory, { squares: newSquares }]);
     setXIsNext((prev) => !prev);
+    setStepNumber(newHistory.length);
   };
 
   //current, history, xIsNext를 이용해 board.js에서 app.js로 옮기기
+
+  const moves = history.map((step, move) => {
+    const desc = move ? "Go to move #" + move : "Go to game start";
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+    //stepNumber가 짝수일때 마다 xIsNext를 true로 설정.
+  };
 
   return (
     <div className="game">
@@ -60,7 +83,8 @@ function App() {
         <Board squares={current.squares} onClick={(i) => handleClick(i)} />
       </div>
       <div className="game-info">
-        <div className="status">{status}</div>
+        <div>{status}</div>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
