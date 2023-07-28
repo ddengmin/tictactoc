1191fec4e60af4a74a5bce2828ab423507018307
ddengmin <ammin1127@naver.com>
2023-07-28 16:29:31 +0900

    ♻️ "board.js -> app.js 코드 리팩토링"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,66 @@
+import { useState } from "react";
 import "./App.css";
 import Board from "./components/Board";
 
 function App() {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [xIsNext, setXIsNext] = useState(true);
+  //board.js에 있는 아이들을 app.js로 옮겨줌(위에 두개)
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+      //승자 경우의 수
+    ];
+    for (let index = 0; index < lines.length; index++) {
+      const [a, b, c] = lines[index];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const current = history[history.length - 1];
+  const winner = calculateWinner(current.squares);
+
+  let status;
+  if (winner) {
+    status = "Winner : " + winner;
+  } else {
+    status = `Next Player : ${xIsNext ? "X" : "O"}`;
+  }
+
+  const handleClick = (i) => {
+    const newSquares = current.squares.slice();
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
+
+    newSquares[i] = xIsNext ? "X" : "O";
+    setHistory([...history, { squares: newSquares }]);
+    setXIsNext((prev) => !prev);
+  };
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board squares={current.squares} onClick={(i) => handleClick(i)} />
       </div>
-      <div className="game-info">game-info</div>
+      <div className="game-info">
+        <div className="status">{status}</div>
+      </div>
     </div>
   );
 }
-
-//app을 가상돔으로 적용 및 바꾸기
-
 export default App;
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,69 +1,14 @@
-import React, { useState } from "react";
 import Square from "./Square";
 import "./Board.css";
 
-const Board = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  //getter, setter
-  const [xIsNext, setxIsNext] = useState(true);
-  //getter, setter
-
-  const calculateWinner = (squares) => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-      //승자 경우의 수
-    ];
-    for (let index = 0; index < lines.length; index++) {
-      const [a, b, c] = lines[index];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
-  };
-
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = "Winner : " + winner;
-  } else {
-    status = "Next Player : " + (xIsNext ? "X" : "O");
-  }
-
-  const handleClick = (i) => {
-    const newSquares = squares.slice();
-    console.log("newSquares", newSquares);
-    console.log("newSquares[i]", newSquares[i]);
-    if (calculateWinner(newSquares) || squares[i]) {
-      return;
-    }
-
-    newSquares[i] = xIsNext ? "X" : "O";
-    setSquares(newSquares);
-    setxIsNext((prev) => !prev);
-    //삼항연산자를 사용 그 이후, 전에 떴던 모양을 안뜨게 해줌
-  };
-  //함수형 컴포넌트로 바꿔줌 #1
-
+const Board = ({ squares, onClick }) => {
   const renderSquare = (i) => {
-    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+    return <Square value={squares[i]} onClick={() => onClick(i)} />;
   };
   //함수형 컴포넌트로 바꿔줌 #2 (밑에 renderSquare도 똑같이 바꿔줌)
 
   return (
     <div>
-      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
